4b55d338342a70eccf8b09512f5da50e335b2030
IgorBrat <akbars5678@gmail.com>
2022-09-03 13:46:17 +0300

    Made better tree printing

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ from red_black_tree import RedBlackTree
 
 def main():
     tree = RedBlackTree()
-
+    tree.unbalanced_insert(10)
+    tree.unbalanced_insert(11)
+    tree.unbalanced_insert(2)
+    tree.unbalanced_insert(3)
+    tree.unbalanced_insert(20)
+    tree.unbalanced_insert(1)
+    tree.unbalanced_insert(15)
+    tree.unbalanced_insert(30)
+    tree.unbalanced_insert(40)
+    tree.unbalanced_insert(18)
+    tree.unbalanced_insert(17)
+    tree.print_tree()
 
 if __name__ == '__main__':
     main()
--- red_black_tree.py
+++ red_black_tree.py
@@ -6,7 +6,33 @@ class RedBlackTree:
     def __init__(self, root: RedBlackNode = None):
         self.root = root
 
-    def print_tree(self, level=0):
-        node = self.root
-        res = ""
+    def unbalanced_insert(self, value: int) -> RedBlackNode:
+        if value is None:
+            raise ValueError("Can`t insert empty node")
+        else:
+            if self.root is None:
+                self.root = RedBlackNode(value)
+                return self.root
+            else:
+                temp = self.root
+                while True:
+                    if temp.value == value:
+                        raise ValueError("Can`t insert duplicate")
+                    elif temp.value > value:
+                        if temp.left is None:
+                            temp.left = RedBlackNode(value)
+                            temp.left.parent = temp
+                            return temp.left
+                        else:
+                            temp = temp.left
+                    else:
+                        if temp.right is None:
+                            temp.right = RedBlackNode(value)
+                            temp.right.parent = temp
+                            return temp.right
+                        else:
+                            temp = temp.right
+
+    def print_tree(self):
+        print(self.root.__str__())
 
