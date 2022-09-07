94e501c26f260df725143d940e6b4417c19ec18a
IgorBrat <akbars5678@gmail.com>
2022-09-07 19:06:41 +0300

    Added testing site in README, changed printing tree function

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ## Task:
 Make deletion of an element in a Red-Black tree.
+
+### [Site for testing](https://www.cs.usfca.edu/~galles/visualization/RedBlack.html)
--- red_black_node.py
+++ red_black_node.py
@@ -9,20 +9,21 @@ class RedBlackNode(Node):
 
     def __str__(self, level=0):
         res = ""
-        if self.left is not None and self.right is not None:
-            res += self.left.__str__(level + 1)
-            res += "\t" * level + f"-> {self.value.__str__()}({self.color.value})\n"
-            res += self.right.__str__(level + 1)
-        elif self.left is not None and self.right is None:
-            res += self.left.__str__(level + 1)
-            res += "\t" * level + f"-> {self.value.__str__()}({self.color.value})\n"
-            res += "\t" * (level + 1) + "-> " + "None(black)" + "\n"
-        elif self.left is None and self.right is not None:
-            res += "\t" * (level + 1) + "-> " + "None(black)" + "\n"
-            res += "\t" * level + f"-> {self.value.__str__()}({self.color.value})\n"
-            res += self.right.__str__(level + 1)
-        else:
-            res += "\t" * level + f"-> {self.value.__str__()}({self.color.value})\n"
+        if self.value != 0:
+            if self.left is not None and self.right is not None:
+                res += self.left.__str__(level + 1)
+                res += "\t" * level + f"-> {self.value.__str__()}({self.color.value})\n"
+                res += self.right.__str__(level + 1)
+            elif self.left is not None and self.right is None:
+                res += self.left.__str__(level + 1)
+                res += "\t" * level + f"-> {self.value.__str__()}({self.color.value})\n"
+                res += "\t" * (level + 1) + "-> " + "None(black)" + "\n"
+            elif self.left is None and self.right is not None:
+                res += "\t" * (level + 1) + "-> " + "None(black)" + "\n"
+                res += "\t" * level + f"-> {self.value.__str__()}({self.color.value})\n"
+                res += self.right.__str__(level + 1)
+            else:
+                res += "\t" * level + f"-> {self.value.__str__()}({self.color.value})\n"
         return res
 
     def __repr__(self):
